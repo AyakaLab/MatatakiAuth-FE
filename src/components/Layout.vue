9d2d1eeecf675b1ba979836cc714240f04130642
Ayaka Neko <neko@ayaka.moe>
2020-11-12 23:48:30 +0800

    :sparkles: Added Layout for global looking
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ src/components/Layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <Header />
+    <div class="slot-shell" id="slot-shell">
+      <slot />
+    </div>
+    <Footer class="footer-container" />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Layout/Header.vue'
+import Footer from '@/components/Layout/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.container {
+  height: 100%;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.slot-shell {
+  flex: 1;
+}
+
+.footer-container {
+  height: 56px;
+  text-align: center;
+}
+</style>
