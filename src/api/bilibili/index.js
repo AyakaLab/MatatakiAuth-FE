0dd21d359dc086b797e100644b999a50baa3b392
Ayaka Neko <neko@ayaka.moe>
2020-11-13 20:48:14 +0800

    :bug: Fixed API endpoint misslead
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/bilibili/index.js
+++ src/api/bilibili/index.js
@@ -3,14 +3,14 @@ import Axios from 'axios'
 const Bilibili = {
   async getQrcode () {
     return new Promise((resolve, reject) => {
-      Axios.get('http://127.0.0.1:8083/bilibili/qrcodeUrl').then(res => {
+      Axios.get('https://auth.matataki.io/api/bilibili/qrcodeUrl').then(res => {
         resolve(res.data)
       })
     })
   },
   async getLoginStatus (hashId) {
     return new Promise((resolve, reject) => {
-      Axios.get('http://127.0.0.1:8083/bilibili/loginStatus', { params: { id: hashId } }).then(res => {
+      Axios.get('https://auth.matataki.io/api/bilibili/loginStatus', { params: { id: hashId } }).then(res => {
         resolve(res.data)
       })
     })
