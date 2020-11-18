0f13094c105eda47c4d260e4c7d149f84e8dd627
Ayaka Neko <neko@ayaka.moe>
2020-11-18 16:58:25 +0800

    :sparkles: 优化 endpoint 配置
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/bilibili/index.js
+++ src/api/bilibili/index.js
@@ -1,16 +1,18 @@
 import Axios from 'axios'
 
+const MATATAKI_AUTH_API = 'https://auth.matataki.io/api'
+
 const Bilibili = {
   async getQrcode () {
     return new Promise((resolve, reject) => {
-      Axios.get('https://auth.matataki.io/api/bilibili/qrcodeUrl').then(res => {
+      Axios.get(MATATAKI_AUTH_API + '/bilibili/qrcodeUrl').then(res => {
         resolve(res.data)
       })
     })
   },
   async getLoginStatus (hashId, userId) {
     return new Promise((resolve, reject) => {
-      Axios.get('https://auth.matataki.io/api/bilibili/loginStatus', { params: { id: hashId, userId } }).then(res => {
+      Axios.get(MATATAKI_AUTH_API + '/bilibili/loginStatus', { params: { id: hashId, userId } }).then(res => {
         resolve(res.data)
       })
     })
