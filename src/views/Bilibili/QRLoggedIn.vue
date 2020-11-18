04fd655fc6bf65ef78fc498c3218406c325bddc2
Ayaka Neko <neko@ayaka.moe>
2020-11-18 15:39:14 +0800

    :sparkles: Added page view prevention
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/views/Bilibili/QRLoggedIn.vue
+++ src/views/Bilibili/QRLoggedIn.vue
@@ -6,10 +6,27 @@
 
 <script>
 import Layout from '@/components/Layout.vue'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     Layout
+  },
+  computed: {
+    ...mapState(['isLoggedIn'])
+  },
+  watch: {
+    isLoggedIn (val) {
+      if (!val) this.$router.push({ name: 'Home' })
+    }
+  },
+  mounted () {
+    if (!this.isLoggedIn) this.$router.push({ name: 'Home' })
+    else {
+      setTimeout(() => {
+        this.$router.push({ name: 'Auth' })
+      }, 3000)
+    }
   }
 }
 </script>
