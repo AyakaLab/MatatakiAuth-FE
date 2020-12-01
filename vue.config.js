612e7f8a581e31558ddae828d5dad19b9528fc30
Ayaka Neko <neko@ayaka.moe>
2020-12-01 15:20:08 +0800

    :green_heart: Update CI scripts
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? ''
-    : './'
+  publicPath: process.env.TESTENV === 'test' ? '/test/' : process.env.NODE_ENV === 'production' ? '././' : './'
 }
