f330400940f7425bb24a70712f7c9d0a06dcda02
Ayaka Neko <neko@ayaka.moe>
2020-12-01 16:56:05 +0800

    :sparkles: 添加两个网络的不同方法
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/bilibili/index.js
+++ src/api/bilibili/index.js
@@ -1,6 +1,7 @@
 import Axios from 'axios'
 
 const MATATAKI_AUTH_API = 'https://auth.matataki.io/api'
+const MATATAKI_AUTH_API_TEST = 'https://auth.matataki.io/apitest'
 
 const Bilibili = {
   async getQrcode () {
@@ -10,9 +11,23 @@ const Bilibili = {
       })
     })
   },
+  async getQrcodeTest () {
+    return new Promise((resolve, reject) => {
+      Axios.get(MATATAKI_AUTH_API_TEST + '/bilibili/qrcodeUrl').then(res => {
+        resolve(res.data)
+      })
+    })
+  },
   async getLoginStatus (hashId, userId) {
     return new Promise((resolve, reject) => {
       Axios.get(MATATAKI_AUTH_API + '/bilibili/loginStatus', { params: { id: hashId, userId } }).then(res => {
+        resolve(res.data)
+      })
+    })
+  },
+  async getLoginStatusTest (hashId, userId) {
+    return new Promise((resolve, reject) => {
+      Axios.get(MATATAKI_AUTH_API_TEST + '/bilibili/loginStatus', { params: { id: hashId, userId } }).then(res => {
         resolve(res.data)
       })
     })
