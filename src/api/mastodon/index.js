953df3c49367943458f2e39bd014fdb1f13f31ba
Ayaka Neko <neko@ayaka.moe>
2020-12-16 19:01:01 +0800

    :sparkles: 添加 API 依赖
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/mastodon/index.js
+++ src/api/mastodon/index.js
@@ -1,17 +1,49 @@
 import Axios from 'axios'
 
-// const MATATAKI_AUTH_API = 'https://auth.matataki.io/api'
+const MATATAKI_AUTH_API = 'https://auth.matataki.io/api'
 // const MATATAKI_AUTH_API_TEST = 'https://auth.matataki.io/apitest'
+const MATATAKI_AUTH_API_TEST = 'http://127.0.0.1:8083'
+
+const instanceModel = {
+  uri: '',
+  title: '',
+  short_description: '',
+  description: '',
+  email: '',
+  version: '',
+  urls: {},
+  stats: {},
+  thumbnail: '',
+  languages: [],
+  registrations: false,
+  approval_required: false,
+  invites_enabled: false,
+  contact_account: {}
+}
+
+const instanceKeys = Object.keys(instanceModel).sort()
 
 const Mastodon = {
   async isMastodonInstance (url) {
-    let instanceData
+    // eslint-disable-next-line no-unused-vars
+    let instanceResData
     try {
-      instanceData = (await Axios.get('http://' + url + '/api/v1/instance')).data
+      instanceResData = (await Axios.get(url + '/api/v1/instance')).data
     } catch {
-      instanceData = (await Axios.get('https://' + url + '/api/v1/instance')).data
+      try {
+        instanceResData = (await Axios.get(url + '/api/v1/instance')).data
+      } catch {
+        return false
+      }
     }
-    return instanceData
+    const instanceResKeys = Object.keys(instanceResData).sort()
+    return JSON.stringify(instanceKeys) === JSON.stringify(instanceResKeys)
+  },
+  async getOAuthToken (token) {
+    return (await Axios.get(MATATAKI_AUTH_API + '/mastodon/oauth/token', { headers: { Authorization: `Bearer ${token}` } })).data
+  },
+  async getOAuthTokenTest (token) {
+    return (await Axios.get(MATATAKI_AUTH_API_TEST + '/mastodon/oauth/token', { headers: { Authorization: `Bearer ${token}` } })).data
   }
 }
 
